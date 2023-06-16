13691433aeaa22abc8170de9931054be3d94c182
imJudah <judahjmdasuki@gmail.com>
2023-06-16 07:35:11 +0700

    feat: install flowbite dan buat menu sidebar, main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import MenuSidebar from './components/MenuSidebar';
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <div className="h-full w-full">
+            <MenuSidebar />
 
-  return (
-    <>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            <div className="p-4 sm:ml-64">
+                <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+                    <div className="grid grid-cols-3 gap-4 mb-4">
+                        <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                        <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                        <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+                        <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                    </div>
+                    <div className="grid grid-cols-2 gap-4 mb-4">
+                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+                        <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                    </div>
+                    <div className="grid grid-cols-2 gap-4">
+                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                        <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                            <p className="text-2xl text-gray-400 dark:text-gray-500">+</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
